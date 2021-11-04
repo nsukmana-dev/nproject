6427ccc48398546da5a68169527defc37da01844
Nandar Sukmana <nsukmana.dev@gmail.com>
2021-11-04 08:44:33 +0700

    halaman error

--- web/handler/user.go
+++ web/handler/user.go
@@ -18,7 +18,8 @@ func NewUserHandler(userService user.Service) *userHandler {
 func (h *userHandler) Index(c *gin.Context) {
 	users, err := h.userService.GetAllUsers()
 	if err != nil {
-		//return users, err
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
 	}
 	// c.JSON(http.StatusOK, users)
 	c.HTML(http.StatusOK, "user_index.html", gin.H{"users": users})
