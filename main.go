08d7f632bff89724f1db814117a46482d695e760
Nandar Sukmana <nsukmana.dev@gmail.com>
2021-11-17 10:58:13 +0700

    form input campaign

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 	transactionHandler := handler.NewTransactionHandler(transactionService)
 
 	userWebHandler := webHandler.NewUserHandler(userService)
-	campaignWebHandler := webHandler.NewCampaignHandler(campaignService)
+	campaignWebHandler := webHandler.NewCampaignHandler(campaignService, userService)
 
 	router := gin.Default()
 	router.Use(cors.Default())
@@ -97,6 +97,7 @@ func main() {
 	router.POST("/users/avatar/:id", userWebHandler.CreateAvatar)
 
 	router.GET("/campaigns", campaignWebHandler.Index)
+	router.GET("/campaigns/new", campaignWebHandler.New)
 
 	router.Run()
 	// router.Run(":8081") //ganti port
--- web/handler/campaign.go
+++ web/handler/campaign.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 	"nproject/campaign"
+	"nproject/user"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -10,10 +11,11 @@ import (
 
 type campaignHandler struct {
 	campaignService campaign.Service
+	userService     user.Service
 }
 
-func NewCampaignHandler(campaignService campaign.Service) *campaignHandler {
-	return &campaignHandler{campaignService}
+func NewCampaignHandler(campaignService campaign.Service, userService user.Service) *campaignHandler {
+	return &campaignHandler{campaignService, userService}
 }
 
 func (h *campaignHandler) Index(c *gin.Context) {
@@ -21,11 +23,28 @@ func (h *campaignHandler) Index(c *gin.Context) {
 	if err != nil {
 		kodeErr := strconv.Itoa(http.StatusInternalServerError)
 		nameErr := "Cannot get campaigns"
-		linkErr := "users"
+		linkErr := "campaigns"
 		errorStatus := ErrorData(kodeErr, nameErr, linkErr)
 		c.HTML(http.StatusInternalServerError, "error.html", errorStatus)
 		return
 	}
 
 	c.HTML(http.StatusOK, "campaign_index.html", gin.H{"campaigns": campaigns})
+}
+
+func (h *campaignHandler) New(c *gin.Context) {
+	users, err := h.userService.GetAllUsers()
+	if err != nil {
+		kodeErr := strconv.Itoa(http.StatusInternalServerError)
+		nameErr := "Cannot get all user"
+		linkErr := "campaigns"
+		errorStatus := ErrorData(kodeErr, nameErr, linkErr)
+		c.HTML(http.StatusInternalServerError, "error.html", errorStatus)
+		return
+	}
+
+	input := campaign.FormCreateCampaignInput{}
+	input.Users = users
+
+	c.HTML(http.StatusOK, "campaign_new.html", input)
 }
